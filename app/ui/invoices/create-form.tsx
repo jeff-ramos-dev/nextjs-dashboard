b788ba1ba16d2bb31edeab7e1f158427a8a14ed7
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2024-04-27 17:10:05 -0700

    Display error messages for create and edit invoice forms

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -134,6 +134,15 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </p>
               ))}
           </div>
+          <div>
+            {state.errors?.amount ||
+            state.errors?.customerId ||
+            state.errors?.status ? (
+              <p className="mt-2 text-sm text-red-500" key={state.message}>
+                {state.message}
+              </p>
+            ) : null}
+          </div>
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
